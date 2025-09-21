1fd8826e97e5a5a776f82d41d981a44907abe2c0
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2025-09-21 23:31:15 +0700

    review:location-api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import checkRoutes from "@root/routes/checkRoutes";
 import userRoutes from "@root/routes/userRoutes";
 import tokenRouter from "@root/routes/tokenRoutes";
+import locationRouter from "@root/routes/locationRoutes";
 const router = Router();
 
 router.use(checkRoutes);
 router.use(userRoutes);
 router.use(tokenRouter);
+router.use(locationRouter);
 
 export default router;
